4480a26ebbcda5acdb72ab65b10ef6552272c003
nileshp-cuelogic <nilesh.patil@cuelogic.co.in>
2016-04-26 11:20:03 +0530

    initial commit

---
+++ HelloWorld/HelloWorldUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Models;
+
+namespace HelloWorldUnitTest
+{
+    [TestClass]
+    public class AccountControllerTester
+    {
+        [TestMethod]
+        public void CreateHelloWorldStringTest()
+        {
+            ClsTester obj = new ClsTester();
+            Assert.AreEqual("Hello World", obj.CreateHelloWorldString());
+               
+        }
+    }
+}
